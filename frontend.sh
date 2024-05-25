877f519eb845f67063632b045fdc9af55751a864
Gaurang Shah <gaurang.shah.1@hotmail.com>
2024-05-25 11:07:27 +0530

    added expense.conf

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,8 @@
 
 yum update -y
 dnf install nginx -y 
+#Copy expense.conf file 
+cp expense.conf /etc/nginx/default.d/expense.conf
 systemctl enable nginx 
 systemctl start nginx 
 firewall-cmd --permanent --add-port=80/tcp
@@ -12,3 +14,4 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+systemctl restart nginx 

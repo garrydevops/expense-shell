1be30e4ce62f705f31cbae1d753adac00b87fdd2
Gaurang Shah <gaurang.shah.1@hotmail.com>
2024-06-02 11:40:20 +0530

    Update backend.sh
    Added firewall rules

--- backend.sh
+++ backend.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+#Pre-requisite : add ports in  firewall
+sudo -i 
+firewall-cmd --permanent --add-port=3306/tcp
+firewall-cmd --permanent --add-port=80/tcp
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --reload
+#Install nodejs:20 & disable other versions 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
+
+yum update
+
+#add user 
 useradd expense
+
+#copy backend service file
 cp backend.service /etc/systemd/system/backend.service
 mkdir /app 
 
@@ -10,9 +24,13 @@ mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip 
 cd /app 
 unzip /tmp/backend.zip
+
+# install dependencies using npm
 npm install 
+
+#install mysql 
 dnf install mysql -y 
-mysql -h 10.0.0.7 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h <Private IP of SQL> -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend 
 systemctl start backend 
